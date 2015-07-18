149d5e8fbff7fa780c611d689fd0ff6bb26a3d2d
Lahiru Udana <lahirukaka@gmail.com>
2015-07-18 14:51:56 +0530

    converting html views to php

---
+++ pages/login.php
@@ -0,0 +1,11 @@
+<article id="loginwrapper">
+	<div class="row fields">
+		<div class="col-md-6 label">UserName : </div>
+		<input class="col-md-6" id="txtusername" type="text" required />
+	</div>
+	<div class="row fields">
+		<div class="col-md-6 label">Password : </div>
+		<input class="col-md-6" id="txtpassword" type="password" required />
+	</div>
+	<input type="button" id="loginbtn" value="Login" />
+</article>
